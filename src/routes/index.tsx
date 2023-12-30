fd48403ad7c1837b9482e8fffa0259d55c2bf024
Alliedschool-admin <139610662+Alliedschool-admin@users.noreply.github.com>
2023-12-30 12:15:46 +0500

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,6 +10,15 @@ export default function Home() {
     website: "https://hummingbirdtechnical.com",
   };
 
+  const plumbingServices = [
+    "Leak Detection and Repair",
+    "Pipe Installation and Repair",
+    "Water Heater Installation and Repair",
+    "Drain Cleaning",
+    "Toilet Repair and Installation",
+    "Fixture Installation",
+  ];
+
   const CONTEXT = "deploy-preview"; // Replace with your actual context
   const BRANCH = "main"; // Replace with your actual branch
 
@@ -17,22 +26,43 @@ export default function Home() {
     <main>
       {CONTEXT === "deploy-preview" && (
         <>
-          <Title>Hummingbird Technical & Cleaning Services UAE.</Title>
-
+          <Title>Hello! This is a preview version of our site.</Title>
+          <p>The branch is {BRANCH}</p>
         </>
       )}
 
       <Title>{companyDetails.name}</Title>
-      <h1>Hummingbird Technical & Cleaning Services</h1>
+      <h1>Hello world!</h1>
       <Counter />
       <p>{companyDetails.description}</p>
       <p>
         Visit{" "}
-       <a href={companyDetails.website} target="_blank">
-  Hummingbird Technical & Cleaning Services
-</a>
+        <a href={companyDetails.website} target="_blank">
+          Hummingbird Technical & Cleaning Services
+        </a>{" "}
         to learn more about our services.
       </p>
+
+      {/* Add the plumbing services section below */}
+      <h2>Our Plumbing Services</h2>
+      <ul>
+        {plumbingServices.map((service, index) => (
+          <li key={index}>
+            <a href={`#${service.replace(/\s+/g, "-").toLowerCase()}`}>
+              {service}
+            </a>
+          </li>
+        ))}
+      </ul>
+
+      <h2>Details of Plumbing Services:</h2>
+      {plumbingServices.map((service, index) => (
+        <div key={index} id={service.replace(/\s+/g, "-").toLowerCase()}>
+          <h3>{service}</h3>
+          {/* Add details for each plumbing service here */}
+          <p>Details for {service}.</p>
+        </div>
+      ))}
     </main>
   );
 }
