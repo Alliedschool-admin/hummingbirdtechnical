d2a7bd9a96d8598cab18da7d02e9bd77cc9890eb
Alliedschool-admin <139610662+Alliedschool-admin@users.noreply.github.com>
2023-12-30 13:03:49 +0500

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -11,65 +11,106 @@ export default function Home() {
   };
 
   const plumbingServices = [
-    "Leak Detection and Repair",
-    "Pipe Installation and Repair",
-    "Water Heater Installation and Repair",
-    "Drain Cleaning",
-    "Toilet Repair and Installation",
-    "Fixture Installation",
+    { name: "Leak Detection and Repair", details: "Detect and repair leaks efficiently." },
+    { name: "Pipe Installation and Repair", details: "Expert installation and repair of pipes." },
+    { name: "Water Heater Installation and Repair", details: "Installation and repair of water heaters." },
+    { name: "Drain Cleaning", details: "Professional drain cleaning services." },
+    { name: "Toilet Repair and Installation", details: "Repair and installation of toilets." },
+    { name: "Fixture Installation", details: "Installation of plumbing fixtures with precision." },
   ];
 
   const CONTEXT = "deploy-preview"; // Replace with your actual context
   const BRANCH = "main"; // Replace with your actual branch
 
   return (
-    <main>
+    <main style={styles.main}>
       {CONTEXT === "deploy-preview" && (
         <>
-          <Title>Preview Version</Title>
-          <p>The branch is {BRANCH}</p>
+          <Title style={styles.title}>Preview Version</Title>
+          <p style={styles.previewText}>The branch is {BRANCH}</p>
         </>
       )}
 
-      <Title>{companyDetails.name}</Title>
-      <h1>Welcome to {companyDetails.name}</h1>
+      <Title style={styles.title}>{companyDetails.name}</Title>
+      <h1 style={styles.heading}>Welcome to {companyDetails.name}</h1>
       <Counter />
 
-      <section>
-        <h2>About Us</h2>
+      <section style={styles.section}>
+        <h2 style={styles.subHeading}>About Us</h2>
         <p>{companyDetails.description}</p>
         <p>
           Visit{" "}
-          <a href={companyDetails.website} target="_blank">
+          <a href={companyDetails.website} target="_blank" style={styles.link}>
             {companyDetails.name}
           </a>{" "}
           to learn more about our services.
         </p>
       </section>
 
-      <section>
-        <h2>Our Plumbing Services</h2>
-        <ul>
+      <section style={styles.section}>
+        <h2 style={styles.subHeading}>Our Plumbing Services</h2>
+        <ul style={styles.list}>
           {plumbingServices.map((service, index) => (
-            <li key={index}>
-              <a href={`#${service.replace(/\s+/g, "-").toLowerCase()}`}>
-                {service}
+            <li key={index} style={styles.listItem}>
+              <a href={`#${service.name.replace(/\s+/g, "-").toLowerCase()}`} style={styles.link}>
+                {service.name}
               </a>
             </li>
           ))}
         </ul>
       </section>
 
-      <section>
-        <h2>Details of Plumbing Services</h2>
+      <section style={styles.section}>
+        <h2 style={styles.subHeading}>Details of Plumbing Services</h2>
         {plumbingServices.map((service, index) => (
-          <div key={index} id={service.replace(/\s+/g, "-").toLowerCase()}>
-            <h3>{service}</h3>
-            {/* Add details for each plumbing service here */}
-            <p>Details for {service}.</p>
+          <div key={index} id={service.name.replace(/\s+/g, "-").toLowerCase()} style={styles.serviceDetails}>
+            <h3 style={styles.subSubHeading}>{service.name}</h3>
+            <p style={styles.paragraph}>{service.details}</p>
           </div>
         ))}
       </section>
     </main>
   );
 }
+
+const styles = {
+  main: {
+    fontFamily: "Arial, sans-serif",
+    margin: "20px",
+  },
+  title: {
+    color: "#333",
+  },
+  heading: {
+    color: "#444",
+    fontSize: "1.5em",
+  },
+  section: {
+    marginBottom: "20px",
+  },
+  subHeading: {
+    color: "#555",
+    fontSize: "1.2em",
+  },
+  list: {
+    listStyleType: "none",
+    padding: "0",
+  },
+  listItem: {
+    marginBottom: "8px",
+  },
+  link: {
+    color: "#007BFF",
+    textDecoration: "none",
+  },
+  serviceDetails: {
+    marginTop: "16px",
+  },
+  subSubHeading: {
+    color: "#333",
+    fontSize: "1.1em",
+  },
+  paragraph: {
+    color: "#666",
+  },
+};
